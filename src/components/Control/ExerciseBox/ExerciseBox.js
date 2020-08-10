82b2e3d5863a60a7555c6a987b7af3f5bf9aab71
Francisco Jose Morales <morales0@umd.edu>
2020-08-09 23:23:48 -0400

    Added more icon to exercise box

--- src/components/Control/ExerciseBox/ExerciseBox.js
+++ src/components/Control/ExerciseBox/ExerciseBox.js
@@ -1,22 +1,25 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import styled, {ThemeContext} from 'styled-components/macro'
 
 import { ReactComponent as AddSVG } from '../../../images/plus.svg'
 import { ReactComponent as MinusSVG } from '../../../images/minus.svg'
+import { ReactComponent as MoreSVG } from '../../../images/3_dots.svg'
 import { Set } from '../..'
 import { Button } from '../..'
 
 const ExerciseBox = ({exercise, removeExercise, addSet, removeSet, updateSet}) => {
+   const [settings, setSettings] = useState(false)
 
    return (
       <div className="exercise_box_container">
+         {settings && <ExerciseControl remove={() => removeExercise(exercise.eid)} complete={() => console.log('complete')}/>}
          <header className="exercise_box_header">
             <h3>{exercise.name}</h3>
             <div>
                <span>{exercise.sets.length}</span>
-               <Button onClick={() => removeExercise(exercise.eid)}>
-                  <MinusIcon width="15px"/>
-               </Button>
+               <Button className="more_toggle" onClick={() => setSettings(!settings)}>
+                  <MoreIcon width="25px"/>
+               </Button>   
             </div>
          </header>
          <div className="exercise_box_content" css={`background: ${props => props.theme.themeValue.offBG};`}>
@@ -50,6 +53,57 @@ const ExerciseBox = ({exercise, removeExercise, addSet, removeSet, updateSet}) =
 }
 
 // Styled UI components
+const ExerciseControl = (props) => {
+   return (
+      <div css={`
+         position: absolute;
+         right: 40px;
+
+         display: flex;
+         flex-flow: column;
+         max-height: 100%;
+         min-width: 150px;
+
+         border: 1px solid #9a9a9a;
+         border-radius: 4px;
+         background: #fefefe;
+
+         overflow: hidden;
+
+         & > button {
+            flex: 1;
+            padding: .5rem .5rem;
+            width: 100%;
+            background: transparent;
+            outline: none;
+            border: none;
+            border-radius: 0;
+            appearance: none;
+            text-align: left;
+            cursor: pointer;
+
+            &:hover {
+               filter: brightness(.9);
+            }
+         }
+
+         /* & > button.remove_exercise {
+            background: #ffc6c4;
+         }
+
+         & > button.complete_exercise {
+            background: #c9ffd7;
+         } */
+
+         & > button + button {
+            border-top: 1px solid #a9a9a9;
+         }
+      `}>
+         <button className="remove_exercise" onClick={props.remove}>Remove</button>
+         <button className="complete_exercise" onClick={props.complete}>Complete</button>
+      </div>
+   )
+}
 
 const PlusIcon = styled(AddSVG)`
 
@@ -58,5 +112,7 @@ const PlusIcon = styled(AddSVG)`
 const MinusIcon = styled(MinusSVG)`
 
 `
+
+const MoreIcon = styled(MoreSVG)``
 
 export default ExerciseBox
